10a453d32b68f7ebe2d9967ed25f2420c5a55044
beenomjee <125212257+beenomjee@users.noreply.github.com>
2023-06-15 19:43:59 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,14 @@ dotenv.config({});
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+app.use(helmet());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(cookieParser());
+app.use(express.json({ limit: "1mb" }));
 app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
 
