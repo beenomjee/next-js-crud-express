adc3948f7c23fc2a089a4fea10835c1209af7e62
beenomjee <125212257+beenomjee@users.noreply.github.com>
2023-06-15 19:41:41 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,13 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { authRouter, todoRouter } from "./routes/index.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config({});
 
 const app = express();
 app.use(helmet());
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "1mb" }));
 
